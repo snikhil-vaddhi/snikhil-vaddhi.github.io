4ed96083f8e44fe4ec3b4fd8113955fbaf85e12c
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-03-16 22:17:07 -0500

    changed padding

--- js/app.js
+++ js/app.js
@@ -37,10 +37,10 @@ scrollLinks.forEach((link) => {
     // if (element) {
     //   const offsetTop = element.offsetTop - navbarHeight;
     if (id === "project") {
-      position += 1900;
+      position += 1950;
       console.log(position);
     } else if (id === "contact") {
-      position += 1800;
+      position += 1900;
       console.log(position);
     }
     window.scrollTo({
